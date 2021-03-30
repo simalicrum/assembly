fb8edd58730fe114532f32e0b466e2b002045272
Robert Reinert <robert.reinert@gmail.com>
2021-03-30 11:19:55 -0700

    blocked out welcome page and header/footer

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <footer>
+        <ul>
+          <li><a href="/about">About</a></li>
+          <li><a href="/news">News</a></li>
+          <li><a href="/blog">Blog</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+        <div>205-523-4153</div>
+    </footer>
+  )
+}
---
+++ components/Header.js
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header>
+        <a href="/">Assembly.biz</a>
+        <ul>
+          <li><a href="/about">About Us</a></li>
+          <li><a href="/services">Services</a></li>
+          <li><a href="/portfolio">Portfolio</a></li>
+          <li><a href="/contact">Contact Us</a></li>
+        </ul>
+        <a href="/login">Login</a>
+    </header>
+  )
+}
